f98531b0751a23366c2203da368eac18443f1f17
Latoya Rhoden <rhoden_latoya@yahoo.com>
2019-11-10 18:34:41 -0500

    Reset URL

--- app.js
+++ app.js
@@ -7,23 +7,24 @@ window.onload = function (){
     httpRequest = new XMLHttpRequest();
     searchBtn = document.getElementsByClassName("btn")[0];
     searchBtn.addEventListener("click", processing); 
-    httpRequest.onreadystatechange = forward;
     
-    httpRequest.open('GET', url);
-    httpRequest.send();
-    
-    function processing(){
+    function processing(event){
+        event.preventDefault();
         dataString = document.getElementById("query").value.trim();
         url += "?query=" + dataString;
-        alert(dataString);
-        alert(response);
-        
+        httpRequest.onreadystatechange = forward;
+        httpRequest.open('GET', url);
+        httpRequest.send();
     }
     function forward(){
         
         if (httpRequest.readyState === XMLHttpRequest.DONE) {
             if (httpRequest.status === 200) {
+                alert(httpRequest.responseText);
                 response = httpRequest.responseText;
+                document.getElementById("result").innerHTML = response;
+                url = "https://5853811f13b3468db0ae9f014813fc21.vfs.cloud9.us-east-1.amazonaws.com/superheroes.php";
+
             }
         }
     }
